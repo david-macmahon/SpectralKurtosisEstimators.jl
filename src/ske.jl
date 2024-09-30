1bb66267986b4c1faa94e2061c83eb673bed1319
David MacMahon <davidm@astro.berkeley.edu>
2024-09-30 15:23:23 -0700

    Update implementation of moment calculations
    The SKEstimator constructor  now uses Float64 for M and N, but requires
    that M, N, and M*N*d are integers.  M and N must be integers because
    their fields in SKEstimator are Int.  M*N*d must be integer because the
    gamma function is implemented as an integer factorial.
    
    A convenience constructor allows Integers to be passed for M and N.

--- src/ske.jl
+++ src/ske.jl
@@ -9,74 +9,49 @@ struct SKEstimator
     u4::Float64
 end
 
-function SKEstimator(M::Integer, N::Integer=1, d::AbstractFloat=1)
-    M >= 2 || error("M ($M) must be > 2")
-    N >= 1 || error("N ($N) must be > 1")
+function SKEstimator(M::Float64, N::Float64=1.0, d::Real=1)
+    isinteger(M) || error("value of M ($M) must be an integer")
+    isinteger(N) || error("value of N ($N) must be an integer")
+    isinteger(M*N*d) || error("value of M*N*d ($(M*N*d)) must be an integer")
+
+    M >= 2 || error("M ($M) must be >= 2")
+    N >= 1 || error("N ($N) must be >= 1")
     d > 0 || error("d ($d) must be > 0")
 try
     Nd = N*d
     MNd = M*Nd
 
-    log_Nd = log(Nd)
-    log_Nd1 = log(Nd+1)
-    log_M = log(M)
-    log_M1 = log(M-1)
-    log_MNd = log(M)+log(Nd)
+    u1=1
 
-    loggammadiv_MNd24 = MNd < 6 ? loggamma(MNd+2)-loggamma(MNd+4) : loggammadiv(2, MNd+2)
-    loggammadiv_MNd26 = MNd < 6 ? loggamma(MNd+2)-loggamma(MNd+6) : loggammadiv(4, MNd+2)
-    loggammadiv_MNd28 = MNd < 6 ? loggamma(MNd+2)-loggamma(MNd+8) : loggammadiv(6, MNd+2)
-
-    u1 = 1
-
-    u2 = exp(
-        log(2) + log_Nd + log_Nd1 + 2log_M + loggammadiv_MNd24
-        -
-        log_M1
+    u2=(
+        (2M^2 * Nd * (1+Nd))
+        /
+        ((M-1) * (6 + 5M*Nd + M^2*Nd^2))
     )
 
-    u3 = exp(
-        log(8) + log_Nd + log_Nd1 + 3log_M + loggammadiv_MNd26
-        -
-        2log_M1
-        +
-        log((Nd+4)*MNd - 5Nd - 2)
+    u3=(
+        (8M^3 * Nd * (Nd+1))
+        * ((-2 + Nd * (-5 + M *(4 + Nd))))
+        / ((M-1)^2 * (MNd+5) * (MNd+4) * (MNd+3) * (MNd+2))
     )
 
-    #=
-    u4 = exp(
-        log(12) + log_Nd + log_Nd1 + 4log_M + loggammadiv_MNd28
-        -
-        3log_M1
-        +
-        log(
-            M^3*Nd^4 + 3M^2*Nd^4 + MNd^3 + 68M^2*Nd^3
-            - 93M*Nd^3 + 125MNd^2 - 245M*Nd^2
-            + 84Nd^2 - 32MNd + 48Nd + 24
-        )
-    )
-    =#
-
-    logu4q = (
-        log(12) + log_Nd + log_Nd1 + 4log_M + loggammadiv_MNd28
-        -
-        3log_M1
+    u4=(
+        (12M^4 * Nd * (Nd+1))
+        * (24 + Nd*(48 + 84Nd + M*(-32 + Nd*(-245 - 93Nd + M*(125 + Nd*(68 + M + (3 + M)*Nd))))))
+        /
+        ((M-1)^3 * (MNd+7) * (MNd+6) * (MNd+5) * (MNd+4) * (MNd+3) * (MNd+2))
     )
 
-    u4 = mapreduce(((s,x),)->s*exp(logu4q+x), +,
-        (
-            (+1, 3log_M+4log_Nd), (+1, log(3)+2log_M+4log_Nd), (+1, 3log_MNd), (+1, log(68)+2log_M+3log_Nd),
-            (-1, log(93)+log_M+3log_Nd), (+1, log(125)+2log_MNd), (-1, log(245)+log_M+2log_Nd),
-            (+1, log(84)+2log_Nd), (-1, log(32)+log_MNd), (+1, log(48)+log_Nd), (+1, log(24))
-        )
-    )
-
-    SKEstimator(intM, intN, d, u1, u2, u3, u4)
+    SKEstimator(M, N, d, u1, u2, u3, u4)
 catch
     @show M N d
     rethrow()
-end    
-end    
+end
+end
+
+function SKEstimator(M::Integer, N::Integer=1, d::Real=1)
+    SKEstimator(float(M), float(N), d)
+end
 
 function B1(u2, u3)
     u3^2 / u2^3
