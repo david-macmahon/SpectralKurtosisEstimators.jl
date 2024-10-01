c079316a21d23c47e61ed18f9cfaa8c3d6260dbf
David MacMahon <davidm@astro.berkeley.edu>
2024-10-01 12:54:53 -0700

    Add comments to test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,14 +21,23 @@ upperiii_expected = 1.282_12
 # Table 1 has -0.71% of u4erriii_expected, but our errors are absolute value
 u4erriii_expected = 0.007_1
 
+# Create SKEstimator for the given paramters
 ske = SKEstimator(M, N, d)
 
+# Get Pearson Type VI "probablity curve" (PDF approximation) for our esitmator
+# along with a measure of the error in the fourth central moment.
 skdvi, u4errvi = @test_logs (:warn,"pearson critereon < 1") pearson_type_vi(ske)
+
+# Get Pearson Type III "probablity curve" (PDF approximation) for our esitmator
+# along with a measure of the error in the fourth central moment.
 skdiii, u4erriii = @test_logs (:warn,"pearson critereon < 1") pearson_type_iii(ske)
 
+# Compute lower and upper thresholds for our "probability curves" corresponding
+# to ±3 sigma of a standard normal distribution.
 lowervi, uppervi = SpectralKurtosisEstimators.thresholds(skdvi, nsigma)
 loweriii, upperiii = SpectralKurtosisEstimators.thresholds(skdiii, nsigma)
 
+# Test that everything agrees with the Table 1 entries to the given precision
 @test lowervi ≈ lowervi_expected atol=0.000_005
 @test uppervi ≈ uppervi_expected atol=0.000_005
 @test u4errvi ≈ u4errvi_expected atol=0.000_05
